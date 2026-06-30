14de5549c5367f2582db464505e8ac0abc96614a
Asraful Islam <iasraful321@gmail.com>
2026-07-01 02:57:18 +0600

    db setup

--- app/database.py
+++ app/database.py
@@ -1,8 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
-DATABASE_URL = "postgresql://postgres:asdf@localhost:5432/review_db"
+DATABASE_URL = os.environ.get("DATABASE_URL", "postgresql://postgres:asdf@localhost:5432/review_db")
+
+if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
