57d10bccad6aeead53101db0d8ab92fecee3811a
Asraful Islam <iasraful321@gmail.com>
2026-07-01 04:55:11 +0600

    added the db and env

--- app/database.py
+++ app/database.py
@@ -2,6 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 DATABASE_URL = os.environ.get("DATABASE_URL", "postgresql://postgres:asdf@localhost:5432/review_db")
 
